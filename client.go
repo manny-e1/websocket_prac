7c473a327ca4ac7cc2877553af0a7c7fb0dc6f7e
manny-e1 <aewnetu6@gmail.com>
2023-03-29 11:53:31 +0300

    event payload and send messages

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 
 	"github.com/gorilla/websocket"
@@ -10,30 +11,69 @@ type ClientList map[*Client]bool
 
 type Client struct {
 	connection *websocket.Conn
-	manager *Manager
+	manager    *Manager
+	egress     chan Event
 }
 
-func NewClient(conn *websocket.Conn, manager *Manager) *Client{
+func NewClient(conn *websocket.Conn, manager *Manager) *Client {
 	return &Client{
 		connection: conn,
-		manager: manager,
+		manager:    manager,
+		egress:     make(chan Event),
 	}
 }
 
 func (c *Client) readMessages() {
-	defer func(){
+	defer func() {
 		c.manager.removeClient(c)
 	}()
 	for {
-		messageType,payload,err := c.connection.ReadMessage()
-	if err != nil {
-		if websocket.IsUnexpectedCloseError(err,websocket.CloseGoingAway,websocket.CloseAbnormalClosure) {
-			log.Printf("error reading message: %v", err)
+		_, payload, err := c.connection.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error reading message: %v", err)
 
+			}
+			break
 		}
-		break
+
+		var request Event
+
+		if err := json.Unmarshal(payload, &request); err != nil {
+			log.Printf("errror marshalling event :%v", err)
+			break
+		}
+
+		if err := c.manager.routeEvent(request, c); err != nil {
+			log.Println("error handling message: ", err)
+		}
 	}
-	log.Println(messageType)
-	log.Println(string(payload))
+}
+
+func (c *Client) writeMessages() {
+	defer func() {
+		c.manager.removeClient(c)
+	}()
+	for {
+		select {
+		case message, ok := <-c.egress:
+			if !ok {
+				if err := c.connection.WriteMessage(websocket.CloseMessage, nil); err != nil {
+					log.Println("connection closed:", err)
+				}
+				return
+			}
+			data, err := json.Marshal(message)
+			if err != nil {
+				log.Println("err")
+				return
+			}
+
+			if err := c.connection.WriteMessage(websocket.TextMessage, data); err != nil {
+				log.Printf("failed to send message: %v", err)
+			}
+			log.Println("message sent")
+		}
+
 	}
 }
--- manager.go
+++ manager.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"log"
 	"net/http"
 	"sync"
@@ -18,10 +20,34 @@ var (
 type Manager struct {
 	clients ClientList
 	sync.RWMutex
+	handlers map[string]EventHandler
 }
 
 func NewManager() *Manager {
-	return &Manager{clients: make(ClientList)}
+	m := &Manager{clients: make(ClientList), handlers: make(map[string]EventHandler)}
+	m.setupEventHandlers()
+	return m
+}
+
+func (m *Manager) setupEventHandlers() {
+	m.handlers[EventSendMessage] = SendMessage
+}
+
+func SendMessage(event Event, c *Client) error {
+	fmt.Println(event)
+	return nil
+}
+
+func (m *Manager) routeEvent(event Event, c *Client) error {
+	if handler, ok := m.handlers[event.Type]; ok {
+		if err := handler(event, c); err != nil {
+			return err
+		}
+		return nil
+	} else {
+		return errors.New("no such event type")
+	}
+
 }
 
 func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
@@ -34,6 +60,7 @@ func (m *Manager) serveWS(w http.ResponseWriter, r *http.Request) {
 	client := NewClient(conn, m)
 	m.addClient(client)
 	go client.readMessages()
+	go client.writeMessages()
 }
 
 func (m *Manager) addClient(client *Client) {
